373e197e002a96b02a5b2119130db13e29a851ee
patelsaheb1219 <patelsaheb1219@gmail.com>
2018-09-12 15:49:10 +0530

    Profile route is complete

--- models/Profile.js
+++ models/Profile.js
@@ -113,11 +113,11 @@ const ProfileSchema = new Schema({
     },
     instagram: {
       type:String
-    },
-    date: {
-      type:Date,
-      default: Date.now
     }
+  },
+  date: {
+    type:Date,
+    default: Date.now
   }
 });
 
