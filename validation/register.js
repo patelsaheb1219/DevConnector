6a84382aed3b3c0d036e0d4f497cf7740f4cb80d
patelsaheb1219 <patelsaheb1219@gmail.com>
2018-09-17 17:32:21 +0530

    Error Handling display

--- validation/register.js
+++ validation/register.js
@@ -27,12 +27,12 @@ module.exports = function validateRegisterInput(data) {
   }
 
   
-  if(Validator.isEmpty(data.password)) {
-    errors.password = 'Password  field is required'; 
-  }
-  
   if(!Validator.isLength(data.password , {min:6 , max:30})){
     errors.password = 'Password must be between 6 and 30 characters';
+  }
+
+  if(Validator.isEmpty(data.password)) {
+    errors.password = 'Password  field is required'; 
   }
 
   if(Validator.isEmpty(data.password2)) {
