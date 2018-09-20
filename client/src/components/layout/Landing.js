372290016c6d907426062c3cbd37de32ce8c188e
patelsaheb1219 <patelsaheb1219@gmail.com>
2018-09-20 12:22:30 +0530

    TextFieldGroup Component

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,18 +1,17 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { PropTypes } from "prop-types";
-import { connect } from "react-redux";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
 
 class Landing extends Component {
-  
   componentDidMount() {
-    if(this.props.auth.isAuthenticated){
+    if (this.props.auth.isAuthenticated) {
       this.props.history.push('/dashboard');
     }
   }
+
   render() {
     return (
-      // Landing -->
       <div className="landing">
         <div className="dark-overlay landing-inner text-light">
           <div className="container">
@@ -20,7 +19,7 @@ class Landing extends Component {
               <div className="col-md-12 text-center">
                 <h1 className="display-3 mb-4">Developer Connector</h1>
                 <p className="lead">
-                  {" "}
+                  {' '}
                   Create a developer profile/portfolio, share posts and get help
                   from other developers
                 </p>
@@ -42,10 +41,10 @@ class Landing extends Component {
 
 Landing.propTypes = {
   auth: PropTypes.object.isRequired
-}
+};
 
-const mapStateToProps = state => {
-  auth : state.auth
-}
+const mapStateToProps = state => ({
+  auth: state.auth
+});
 
 export default connect(mapStateToProps)(Landing);
