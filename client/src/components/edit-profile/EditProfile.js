d8d86eeb4727a9bff0660a2a60b2b912e0fee77d
patelsaheb1219 <patelsaheb1219@gmail.com>
2018-09-21 18:02:09 +0530

    Add Education component

--- client/src/components/edit-profile/EditProfile.js
+++ client/src/components/edit-profile/EditProfile.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
@@ -195,6 +196,9 @@ class CreateProfile extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
+              <Link to="/dashboard" className="btn btn-light">
+                Go back
+              </Link>
               <h1 className="display-4 text-center">Edit Profile</h1>
 
               <small className="d-block pb-3">* = required fields</small>
