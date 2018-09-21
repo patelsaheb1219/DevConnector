60b666051ec189ff67f79730532bf93b8e8008ff
patelsaheb1219 <patelsaheb1219@gmail.com>
2018-09-21 11:59:37 +0530

    Profile Action component Delete account

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,48 +1,74 @@
-import axios from 'axios';
-import { GET_PROFILE , PROFILE_LOADING , CLEAR_CURRENT_PROFILE , GET_ERRORS } from './types';
+import axios from "axios";
+import {
+  GET_PROFILE,
+  PROFILE_LOADING,
+  CLEAR_CURRENT_PROFILE,
+  GET_ERRORS,
+  SET_CURRENT_USER
+} from "./types";
 
 //Get current profile
 export const getCurrentProfile = () => dispatch => {
   dispatch(setProfileLoading());
-  axios.get('/api/profile')
-    .then( res => 
+  axios
+    .get("/api/profile")
+    .then(res =>
       dispatch({
-        type : GET_PROFILE,
-        payload : res.data
+        type: GET_PROFILE,
+        payload: res.data
       })
     )
-    .catch( err => 
+    .catch(err =>
       dispatch({
-        type : GET_PROFILE,
-        payload : {}
+        type: GET_PROFILE,
+        payload: {}
       })
     );
-}
+};
 
 // Create profile
-export const createProfile = ( profieData , history ) => dispatch => {
+export const createProfile = (profieData, history) => dispatch => {
   axios
-  .post('/api/profile' , profieData)
-  .then( res => history.push('/dashboard') )
-  .catch(err => 
-    dispatch({ 
-      type: GET_ERRORS,
-      payload: err.response.data
-      
-     }) 
-  ) 
-}
+    .post("/api/profile", profieData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
 
-// Profile loading 
+//Delete account and profile
+export const deleteAccount = () => dispatch => {
+  if (window.confirm("Are you sure? This can NOT be undone!")) {
+    axios
+      .delete("/api/profile")
+      .then(res =>
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {}
+        })
+      )
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      );
+  }
+};
+
+// Profile loading
 export const setProfileLoading = () => {
   return {
-    type : PROFILE_LOADING
-  }
-}
+    type: PROFILE_LOADING
+  };
+};
 
-// Clear Profile 
+// Clear Profile
 export const clearCurrentProfile = () => {
   return {
-    type : CLEAR_CURRENT_PROFILE
-  }
-}
+    type: CLEAR_CURRENT_PROFILE
+  };
+};
