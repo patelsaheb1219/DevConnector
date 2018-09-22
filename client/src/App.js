0ec47be6cb4fa089f27c8cff6a811c491951349a
patelsaheb1219 <patelsaheb1219@gmail.com>
2018-09-22 12:48:34 +0530

    Profile By handle sub Components

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,7 @@ import EditProfile from "./components/edit-profile/EditProfile";
 import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -57,6 +58,8 @@ class App extends Component {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:handle" component={Profile} />
+
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
@@ -74,7 +77,7 @@ class App extends Component {
                   component={EditProfile}
                 />
               </Switch>
-              
+
               <Switch>
                 <PrivateRoute
                   exact
@@ -82,7 +85,7 @@ class App extends Component {
                   component={AddExperience}
                 />
               </Switch>
-              
+
               <Switch>
                 <PrivateRoute
                   exact
