ad778318a5e0382b2e0fe9f55842ba25ab7222c2
patelsaheb1219 <patelsaheb1219@gmail.com>
2018-09-22 11:38:52 +0530

    Profile Component getProfiles

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,7 @@ import { clearCurrentProfile } from "./actions/profileActions";
 import EditProfile from "./components/edit-profile/EditProfile";
 import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
+import Profiles from "./components/profiles/Profiles";
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -55,6 +56,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/profiles" component={Profiles} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
--- client/src/reducers/profileReducer.js
+++ client/src/reducers/profileReducer.js
@@ -1,30 +1,40 @@
-import { GET_PROFILE , PROFILE_LOADING , CLEAR_CURRENT_PROFILE } from '../actions/types';
-
+import {
+  GET_PROFILE,
+  GET_PROFILES,
+  PROFILE_LOADING,
+  CLEAR_CURRENT_PROFILE
+} from "../actions/types";
 
 const intialState = {
   profile: null,
   profiles: null,
   loading: false
-}
+};
 
-export default function( state = intialState , action ) {
-  switch(action.type) {
+export default function(state = intialState, action) {
+  switch (action.type) {
     case PROFILE_LOADING:
       return {
         ...state,
-        loading : true 
+        loading: true
       };
     case GET_PROFILE:
       return {
         ...state,
-        profile:action.payload,
-        loading : false
-      };  
+        profile: action.payload,
+        loading: false
+      };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles : action.payload,
+        loading:false
+      };
     case CLEAR_CURRENT_PROFILE:
       return {
         ...state,
-        profile:null 
-      }  
+        profile: null
+      };
     default:
       return state;
   }
